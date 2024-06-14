ee04f20cad60b8597e9b047993fb18cad65564b5
Riya822 <144415293+Riya822@users.noreply.github.com>
2024-06-14 09:49:18 +0530

    Create ques11.py
    Question 11

---
+++ ques11.py
@@ -0,0 +1,9 @@
+#Write a python program that generates the first n numbers in the Fibonacci sequence
+num=int(input("Enter a number :"))
+n1,n2=0,1
+print(n1,n2,end=" ")
+for i in range(2, num):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3, end=" ")
