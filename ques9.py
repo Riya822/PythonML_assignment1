b8a99fda4555b495c641f0a5768e27f338ea74f9
Riya822 <144415293+Riya822@users.noreply.github.com>
2024-06-14 09:38:22 +0530

    Create ques9.py
    Question 9

---
+++ ques9.py
@@ -0,0 +1,7 @@
+#Write a python program that checks if a substring is present in a given string
+s=input("Enter a string :")
+sub=input("Enter a sub string to be checked :")
+if sub in s:
+    print("yes")
+else:
+    print("no")
