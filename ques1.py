fc1a2e57f80a9b12a99e881853c5e05f6ea7895a
Riya822 <144415293+Riya822@users.noreply.github.com>
2024-06-14 09:11:29 +0530

    Create ques1.py
    Question 1

---
+++ ques1.py
@@ -0,0 +1,4 @@
+#Write a program that takes two numbers as input and prints their sum
+n1=int(input("Enter first number :"))
+n2=int(input("Enter second number :"))
+print("Sum of the numbers:",n1+n2)
