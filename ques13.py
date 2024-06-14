be9fe7b082aa35379bd692ac3eb2677f414b5848
Riya822 <144415293+Riya822@users.noreply.github.com>
2024-06-14 10:05:31 +0530

    Create ques13.py
    Question 13

---
+++ ques13.py
@@ -0,0 +1,4 @@
+#Write a program that asks the user for their birth year and calculates their age 
+year=int(input("Enter your birth year :"))
+current_year=int(input("Enter current year :"))
+print("Your age is :",current_year-year)
