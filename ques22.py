7275d21b504bc190daaae37c05039c47628dac11
Riya822 <144415293+Riya822@users.noreply.github.com>
2024-06-16 17:21:03 +0530

    Create ques22.py
    Question 22

---
+++ ques22.py
@@ -0,0 +1,4 @@
+#Write a python program that returns the minimum and maximum values in a list of numbers
+lst=[5,89,366,45,10,78,42,2]
+print("Maximum number :",max(lst))
+print("Minimum number :",min(lst))
